71b5a8f3ac41a0fa55d478f2363003e102fca160
Aaron <abergman@student.42.fr>
2025-03-20 22:58:22 +0100

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Head from "next/head";
 import PageWrapper from "../layout/PageWrapper/PageWrapper";
 import { demoPagesMenu } from "../menu";
